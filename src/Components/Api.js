5e59b03dc94e3c40ab340e30c309d8c827bc1186
Rajnish33 <youremail@example.com>
2025-12-07 18:11:12 +0530

    update in auth.js

--- src/Components/Api.js
+++ src/Components/Api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const baseURL = (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1")
-  ? "http://localhost:3177/"
-  : "https://red-hope-backend.vercel.app/";
+// const baseURL = "http://localhost:3177/";
+const baseURL = "https://red-hope-backend.vercel.app/";
 
 export default axios.create({ baseURL: baseURL });
